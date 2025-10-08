d1b786c8f78068f6b519758748227e898793a42b
Vladimir Tolstov <tolstov@gmail.com>
2025-10-08 12:01:33 +0700

    Initial commit

---
+++ src/main/java/org/filestorage/app/FileStorageApplication.java
@@ -0,0 +1,13 @@
+package org.filestorage.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageApplication.class, args);
+    }
+
+}
