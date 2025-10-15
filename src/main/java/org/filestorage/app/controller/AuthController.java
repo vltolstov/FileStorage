902793017032776a8905a2d47db936506ec0389a
Vladimir Tolstov <tolstov@gmail.com>
2025-10-15 16:35:42 +0700

    added sign-in sign-out

--- src/main/java/org/filestorage/app/controller/AuthController.java
+++ src/main/java/org/filestorage/app/controller/AuthController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.filestorage.app.dto.UserRequest;
 import org.filestorage.app.dto.UserResponse;
+import org.filestorage.app.exception.UserNotAuthorizedException;
 import org.filestorage.app.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,65 +24,56 @@ public class AuthController {
 
     private final UserService userService;
 
-
-//    Тело запроса (application/json):
-//    {
-//        "username": "user_1",
-//            "password": "password"
-//    }
-//
-//    Ответ в случае успеха: 201 Created со следующим телом:
-//    {
-//        "username": "user_1"
-//    }
     @PostMapping("/auth/sign-up")
-    public ResponseEntity<Map <String, String>> signUp(@Valid @RequestBody UserRequest userRequest, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+    public ResponseEntity<Map <String, String>> signUp(@Valid @RequestBody UserRequest userRequest, HttpServletRequest request, HttpServletResponse response) {
         UserResponse userResponse = userService.create(userRequest);
 
-        HttpSession session = httpServletRequest.getSession();
+        HttpSession session = request.getSession();
         session.setAttribute("username", userRequest.getUsername());
 
         Cookie cookie = new Cookie("JSESSIONID", session.getId());
         cookie.setHttpOnly(true);
         cookie.setPath("/");
-        httpServletResponse.addCookie(cookie);
+        response.addCookie(cookie);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(Map.of("username", userResponse.getUsername()));
     }
 
-
-//    Тело запроса (application/json):
-//    {
-//        "username": "user_1",
-//            "password": "password"
-//    }
-//
-//    Тело ответа в случае успеха (200 OK):
-//    {
-//        "username": "user_1"
-//    }
     @PostMapping("/auth/sign-in")
-    public void signIn(String name, String password) {
-        System.out.println("Sign in");
-//        Коды ошибок:
-//
-//        400 - ошибки валидации (пример - слишком короткий username)
-//        401 - неверные данные (такого пользователя нет, или пароль неправильный)
-//        500 - неизвестная ошибка
+    public ResponseEntity<Map<String, String>> signIn(@Valid @RequestBody UserRequest userRequest, HttpServletRequest request, HttpServletResponse response) {
+        UserResponse userResponse = userService.getUser(userRequest);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("username", userRequest.getUsername());
+
+        Cookie cookie = new Cookie("JSESSIONID", session.getId());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(Map.of("username", userResponse.getUsername()));
     }
 
-
-
-//    Тела запроса нет.
-//    Тело ответа в случае успеха (204 No Content) пустое.
-//    Коды ошибок:
-//            401 - запрос исполняется неавторизованным юзером
-//    500 - неизвестная ошибка
     @PostMapping("/auth/sign-out")
-    public void signOut(Long id) {
+    public ResponseEntity<Void> signOut(HttpServletRequest request, HttpServletResponse response) {
 
+        if(request.getSession().getAttribute("username") == null) {
+            throw new UserNotAuthorizedException("user not authorized");
+        }
+
+        request.getSession().removeAttribute("username");
+        Cookie cookie = new Cookie("JSESSIONID", "");
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 
 }
--- src/main/java/org/filestorage/app/service/UserService.java
+++ src/main/java/org/filestorage/app/service/UserService.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.filestorage.app.dto.UserRequest;
 import org.filestorage.app.dto.UserResponse;
 import org.filestorage.app.exception.UserAlreadyExistException;
+import org.filestorage.app.exception.InvalidCredentialException;
 import org.filestorage.app.mapper.UserMapper;
 import org.filestorage.app.model.User;
 import org.filestorage.app.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -31,8 +35,18 @@ public class UserService {
         return userMapper.toResponse(user);
     }
 
-    public void getUser(Long id) {
+    public UserResponse getUser(UserRequest userRequest) {
+        if(!userRepository.existsByName(userRequest.getUsername())) {
+            throw new InvalidCredentialException("Username or password incorrect");
+        }
 
+        User user = userRepository.getByName(userRequest.getUsername());
+
+        if (!passwordEncoder.matches(userRequest.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialException("Username or password incorrect");
+        }
+
+        return userMapper.toResponse(user);
     }
 
     public void update(Long id, String name) {
