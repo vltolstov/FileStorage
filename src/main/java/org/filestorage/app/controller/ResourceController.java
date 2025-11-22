55429602c2723ced186e77a8ae1e85c1a414570e
Vladimir Tolstov <tolstov@gmail.com>
2025-11-22 16:12:10 +0700

    added downloading resource

--- src/main/java/org/filestorage/app/controller/ResourceController.java
+++ src/main/java/org/filestorage/app/controller/ResourceController.java
@@ -1,5 +1,6 @@
 package org.filestorage.app.controller;
 
+import io.minio.GetObjectResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,13 +12,16 @@ import org.filestorage.app.mapper.ResourceDataResponseMapper;
 import org.filestorage.app.model.MinioResource;
 import org.filestorage.app.model.User;
 import org.filestorage.app.service.MinioService;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -67,6 +71,26 @@ public class ResourceController {
                 .build();
     }
 
+    @Operation(summary = "Скачивание ресурса", description = "Возвращает бинарное содержимое с Content-Type: application/octet-stream")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ресурс найден. Скачивание"),
+            @ApiResponse(responseCode = "400", description = "Ошибка валидации"),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован"),
+            @ApiResponse(responseCode = "404", description = "Ресурс не найден"),
+            @ApiResponse(responseCode = "500", description = "Неизвестная ошибка")
+    })
+    @GetMapping("/resource/download")
+    public ResponseEntity<StreamingResponseBody> downloadResource(@RequestParam String path, @AuthenticationPrincipal User user){
+        pathValidation(path);
+        prefixValidation(path, user.getId());
+
+        StreamingResponseBody streamResponse = minioService.downloadResource(path, user.getId());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(streamResponse);
+    }
+
     private void pathValidation(String path) {
         if(path == null || path.isBlank()){
             throw new PathNotValidException("Path not valid");
@@ -76,7 +100,7 @@ public class ResourceController {
     private void prefixValidation(String path, Long userId){
         if(path.endsWith("/")){
             if(!minioService.isFolderExist(path, userId)){
-                throw new ResourceNotFoundException("Folder " + path + " not found");
+                throw new ResourceNotFoundException("Directory " + path + " not found");
             }
         } else {
             if(!minioService.isFileExist(path, userId)){
@@ -85,15 +109,6 @@ public class ResourceController {
         }
 
     }
-
-
-    //Удаление ресурса
-    //
-    //DELETE /resource?path=$path
-
-    //Скачивание ресурса
-    //
-    //GET /resource/download?path=$path
 
     //Переименование/перемещение ресурса
     //
--- src/main/java/org/filestorage/app/service/MinioService.java
+++ src/main/java/org/filestorage/app/service/MinioService.java
@@ -1,13 +1,6 @@
 package org.filestorage.app.service;
 
-import io.minio.ListObjectsArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.RemoveObjectArgs;
-import io.minio.RemoveObjectsArgs;
-import io.minio.Result;
-import io.minio.StatObjectArgs;
-import io.minio.StatObjectResponse;
+import io.minio.*;
 import io.minio.messages.DeleteError;
 import io.minio.messages.DeleteObject;
 import io.minio.messages.Item;
@@ -17,11 +10,16 @@ import org.filestorage.app.model.MinioResource;
 import org.filestorage.app.util.ResourceType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -135,6 +133,71 @@ public class MinioService {
         }
     }
 
+    public StreamingResponseBody downloadResource(String path, Long userId){
+        if(path.endsWith("/")){
+            return downloadDirectory(path, userId);
+        } else {
+            return downloadFile(path, userId);
+        }
+    }
+
+    private StreamingResponseBody downloadDirectory(String path, Long userId){
+        try {
+            Iterable<Result<Item>> results = minioClient.listObjects(
+                    ListObjectsArgs.builder()
+                            .bucket(defaultBucket)
+                            .prefix(getUserPrefix(userId) + path)
+                            .recursive(true)
+                            .build()
+            );
+
+            StreamingResponseBody stream = outputStream -> {
+                try (ZipOutputStream zipOut = new ZipOutputStream(outputStream)) {
+                    for (Result<Item> result : results) {
+                        Item item = result.get();
+                        if (item.isDir()) continue;
+
+                        try (InputStream input = minioClient.getObject(
+                                GetObjectArgs.builder()
+                                        .bucket(defaultBucket)
+                                        .object(item.objectName())
+                                        .build()
+                        )) {
+                            ZipEntry entry = new ZipEntry(item.objectName().substring((getUserPrefix(userId) + path).length()));
+                            zipOut.putNextEntry(entry);
+                            input.transferTo(zipOut);
+                            zipOut.closeEntry();
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new MinioOperationException("Error zip processing " + path);
+                }
+            };
+            return stream;
+        } catch (Exception e) {
+            throw new MinioOperationException("Error downloading directory " + path);
+        }
+    }
+
+    private StreamingResponseBody downloadFile(String path, Long userId){
+        try {
+            GetObjectResponse object = minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(defaultBucket)
+                            .object(getUserPrefix(userId) + path)
+                            .build()
+            );
+
+            return outputStream -> {
+                try (InputStream input = object){
+                    input.transferTo(outputStream);
+                }
+            };
+        } catch (Exception e) {
+            throw new MinioOperationException("Error downloading file " + path);
+        }
+    }
+
     private List<MinioResource> getDirectoryResources(String path, Long userId){
         return List.of(new MinioResource());
     }
@@ -206,10 +269,6 @@ public class MinioService {
         int slashIndex = sourcePath.lastIndexOf("/");
         return slashIndex < 1 ? "/" : sourcePath.substring(0, slashIndex + 1);
     }
-
-    //Удаление ресурса
-    //
-    //DELETE /resource?path=$path
 
     //Скачивание ресурса
     //
