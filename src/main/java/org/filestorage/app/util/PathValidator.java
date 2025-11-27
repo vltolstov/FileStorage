b3d29785c57453367f4cd1dcdf8f194eb42bdc23
Vladimir Tolstov <tolstov@gmail.com>
2025-11-27 11:55:49 +0700

    added pathValidator

---
+++ src/main/java/org/filestorage/app/util/PathValidator.java
@@ -0,0 +1,34 @@
+package org.filestorage.app.util;
+
+import lombok.RequiredArgsConstructor;
+import org.filestorage.app.exception.PathNotValidException;
+import org.filestorage.app.exception.ResourceNotFoundException;
+import org.filestorage.app.service.MinioService;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PathValidator {
+
+    private final MinioService minioService;
+
+    public void pathValidation(String path) {
+        if(path == null || path.isBlank()){
+            throw new PathNotValidException("Path not valid");
+        }
+    }
+
+    public void prefixValidation(String path, Long userId){
+        if(path.endsWith("/")){
+            if(!minioService.isDirectoryExist(path, userId)){
+                throw new ResourceNotFoundException("Directory " + path + " not found");
+            }
+        } else {
+            if(!minioService.isFileExist(path, userId)){
+                throw new ResourceNotFoundException("File " + path + " not found");
+            }
+        }
+
+    }
+
+}
